df21141300198678a26cd5d8d75d07780427e7cf
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-11-03 16:50:55 -0600

    PresidentFileDAO added unimplemented methods

--- src/data/PresidentFileDAO.java
+++ src/data/PresidentFileDAO.java
@@ -1,5 +1,23 @@
 package data;
 
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
+
 public class PresidentFileDAO implements PresidentDAO {
+	private static final String filename = "WEB-INF/stocks.txt";
+	private ServletContext servletContext;
+	private ArrayList<President> presidents;
+	
+	
+	@Override
+	public ArrayList<President> getAllPresidents() {
+		return presidents;
+	}
+
+	@Override
+	public President getPresidentByTerm(int termNumber) {
+		return presidents.get(termNumber-1);
+	}
 
 }
